14b683ff60f5c5c0f903086e6e3c1764d0565f42
Kachielite <40024356+Kachielite@users.noreply.github.com>
2022-11-07 21:49:58 +0100

    Added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import { Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-cyan-600">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
   );
 }
 
