f11fb963928ef6ec7dadd96c77f57f26cdb379fe
Kachielite <40024356+Kachielite@users.noreply.github.com>
2022-11-12 11:09:05 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,11 +9,24 @@ const app = express();
 const port = process.env.PORT;
 
 //Middleware
+app.use(cors());
+app.use((req, res, next) => {
+  userId = req.userId;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Origin"
+  );
+  next();
+});
 app.use(bodyParser.json());
 
-
 //routes
-app.use("/v1", cors())
+app.use("/v1", cors());
 app.use("/v1", cors(), authRoute);
 app.get("/", (req, res, next) => {
   res.status(200).json({ message: "server is responding well" });
