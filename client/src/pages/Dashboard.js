fd79e0495ff6b845af0e459fab8b123e1ca082b0
Kachielite <40024356+Kachielite@users.noreply.github.com>
2022-11-15 19:33:02 +0100

    Made sidebar visible default

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -6,7 +6,7 @@ import TopBar from "../components/Topbar";
 import Footer from "../components/Footer";
 
 const Dashboard = () => {
-  const [showSideBar, setShowSideBar] = useState(false);
+  const [showSideBar, setShowSideBar] = useState(true);
   // const ref = useRef(null);
 
   // useClickAway(ref, () => {
--- client/src/pages/MenuPages/DashboardTab.js
+++ client/src/pages/MenuPages/DashboardTab.js
@@ -5,7 +5,7 @@ import ProfitStats from "../../components/profit/ProfitStats";
 
 const DashboardTab = () => {
   return (
-    <div className="w-full mb-8">
+    <div className="w-full mb-24">
       <div className="w-full flex flex-col py-12 px-3 md:px-8 space-y-8">
         <Statistics />
         <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between h-full bg-bgWhite">
